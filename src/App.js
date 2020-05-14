87c9ff224bc3a9b1dc0566794e1a54ae6865e723
Hélio Luiz Monteiro Júnior <heliojr.monteiro@yahoo.com.br>
2020-05-14 17:16:37 -0300

    componente header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Header from "./components/Header";
+import './styles.css';
 
+/* 
 function App() {
   return (
     <div className="App">
@@ -7,5 +10,14 @@ function App() {
     </div>
   );
 }
+*/
+
+//Redução da função acima para uma variável
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+)
+
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+//Componente
+//class Header extends Components {
+//    render() {
+//        return <h1>Hello</h1>
+//    }
+//}
+
+//stateless components
+const Header = () => <header id="main-header">JSHunt</header>;
+
+export default Header;
